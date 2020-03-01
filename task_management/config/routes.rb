5784a05bcbf8be30eec72c56baa1ff9527085d4f
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2020-03-01 22:27:48 +0900

    development環境のmail招待で使用するletter_openerをインストールしsetupした

--- task_management/config/routes.rb
+++ task_management/config/routes.rb
@@ -18,4 +18,6 @@ Rails.application.routes.draw do
   resources :projects, only: %i(index)
 
   root to: "dashboards#index"
+
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
