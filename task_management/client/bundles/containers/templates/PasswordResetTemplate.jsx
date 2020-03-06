09df19d0dbb35da7c55780a8df50c880c8bccc78
yusuke-eto <eto.yusuke@moneyforward.co.jp>
2020-03-06 09:32:41 +0900

    ✨ Password再設定画面を一旦作成

--- task_management/client/bundles/containers/templates/PasswordResetTemplate.jsx
+++ task_management/client/bundles/containers/templates/PasswordResetTemplate.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Header } from "../../components/organisms/Header";
+import { PasswordResetForm } from "../../components/organisms/PasswordResetForm";
 
 export const PasswordResetTemplate = ({workspaceMember}) => {
   return (
     <>
       <Header />
+      <PasswordResetForm />
     </>
   );
 }
